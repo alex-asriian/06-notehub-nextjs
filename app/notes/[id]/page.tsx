fb9ea40dc09638c47477b0f98dbbed6118b6044a
Oleksii <alexasr00798@gmail.com>
2026-06-05 17:51:09 +0300

    v.1

---
+++ app/notes/[id]/page.tsx
@@ -0,0 +1,28 @@
+// app/notes/[id]/page.tsx
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query'
+import { fetchNoteById } from '@/src/lib/api'
+import NoteDetailsClient from './NoteDetails.client'
+
+export default async function NoteDetailsPage({
+  params,
+}: {
+  params: { id: string }
+}) {
+  const { id } = await params
+  const queryClient = new QueryClient()
+
+  await queryClient.prefetchQuery({
+    queryKey: ['note', id],
+    queryFn: () => fetchNoteById(id),
+  })
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient />
+    </HydrationBoundary>
+  )
+}
---
+++ app/notes/page.tsx
@@ -0,0 +1,22 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query'
+import { fetchNotes } from '@/src/lib/api'
+import NotesClient from '@/app/notes/NotesClient'
+
+export default async function NotesPage() {
+  const queryClient = new QueryClient()
+
+  await queryClient.prefetchQuery({
+    queryKey: ['notes'],
+    queryFn: () => fetchNotes(),
+  })
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotesClient />
+    </HydrationBoundary>
+  )
+}
